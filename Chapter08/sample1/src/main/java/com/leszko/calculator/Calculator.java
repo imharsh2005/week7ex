8affcf9eddf05321e5ab0990d33f4fc805330770
imharsh2005 <62921022+imharsh2005@users.noreply.github.com>
2021-11-06 23:44:06 -0400

    add Div class

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,14 +3,33 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-/**
-* @author Some javadoc. // OK
-*/
+/** Calculator logic */
 @Service
 public class Calculator {
-        final static int umlNUMBER1 = 3;
 	@Cacheable("sum")
 	public int sum(int a, int b) {
 		return a + b;
 	}
+
+        @Cacheable("div")
+        public int div(int a, int b) {
+                return a / b;
+        }
 }
+
+// package com.leszko.calculator;
+
+// import org.springframework.cache.annotation.Cacheable;
+// import org.springframework.stereotype.Service;
+
+// /**
+// * @author Some javadoc. // OK
+// */
+// @Service
+// public class Calculator {
+//         final static int umlNUMBER1 = 3;
+// 	@Cacheable("sum")
+// 	public int sum(int a, int b) {
+// 		return a + b;
+// 	}
+// }
